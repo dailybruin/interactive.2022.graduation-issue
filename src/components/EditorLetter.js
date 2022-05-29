010877163da32b3d75213112feb317e87bdf3050
Tracy Zhao <75452724+TracyZhao24@users.noreply.github.com>
2022-05-29 00:48:14 -0700

    need to do top border

---
+++ src/components/EditorLetter.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+import { mediaQueries } from '../shared/config';
+
+import leavesBorder from '../images/leavesBorder.svg';
+
+
+const Container = styled.div`
+    position: relative;
+    margin: auto;
+    width: 80%;
+    height: fit-content;
+    block-size: fit-content;
+    border: 8px solid #C4C4C4;
+    font-family: 'Courier Prime', monospace;
+    padding-bottom: 50px;
+
+    ${mediaQueries.mobile} {
+        width: 90%;
+    }
+`
+
+const Header = styled.div`
+    text-align: center;
+    text-transform: uppercase;
+    font-weight: 700;
+    font-size: 45px;
+    line-height: 51px;
+    text-shadow: 0px 4px 4px rgba(82, 79, 64, 0.15);
+    color: #785887;
+    padding: 30px;
+    margin-top: 80px;
+`
+
+const Content = styled.div`
+    margin: auto;
+    width: 70%;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 36px;
+    text-align: justify;
+    margin-bottom: 50px;
+`
+
+const SignOff = styled.div`
+    margin: auto;
+    width: 70%;
+    font-weight: 400;
+    font-size: 32px;
+    line-height: 36px;
+    text-align: justify;
+`
+
+const BottomBorder = styled.img`
+    position: absolute;
+    top: 32%;
+    right: -13%;
+`
+
+export default function EditorLetter(props)
+{
+    return (
+        <Container>
+            <Header>Letter From the Editors</Header>
+            <Content>{props.editor_letter}</Content>
+            <SignOff> warmly, </SignOff>
+            <SignOff>the editors & co</SignOff>
+            <BottomBorder src = {leavesBorder} />
+        </Container>
+    )
+}
